66ab5165869952ebde0ca5c2da11cc954707ad0a
Austin Cox <austin.ed.cox@gmail.com>
2020-03-18 19:27:39 -0700

    added schema and seeds files

---
+++ schema.sql
@@ -0,0 +1,29 @@
+create database if not exists employees_db;
+
+use employees_db;
+
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees(
+	id INT NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	role_id INT,
+	manager_id INT,
+	PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role(
+	id INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(30),
+	salary DECIMAL,
+	department_id INT,
+	PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30),
+	PRIMARY KEY(id)
+);
